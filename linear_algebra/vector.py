92a92c1c05bdf94edb484713ef1d79bbb1c2a1c1
akagiyuu <khanghuynh0245@gmail.com>
2024-09-08 18:58:40 +0700

    feat(linear-algebra): add vector str

--- linear_algebra/vector.py
+++ linear_algebra/vector.py
@@ -1,10 +1,26 @@
+from __future__ import annotations
+from typing import override
+
+
 class Vector:
     data: list[float]
 
     def __init__(self, data: list[float]) -> None:
         self.data = data
 
+    def __setitem__(self, key: int, value: float) -> None:
+        self.data[key] = value
+
+    def __getitem__(self, key: int) -> float:
+        return self.data[key]
+
+    @override
+    def __str__(self) -> str:
+        return str(self.data)
+
+    @override
+    def __repr__(self) -> str:
+        return str(self)
+
     def norm(self, p: float) -> float:
         return sum([abs(x) ** p for x in self.data]) ** (1 / p)
-
-
